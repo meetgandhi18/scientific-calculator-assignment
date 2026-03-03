370b6fbd8f84fe0157ce6015963c69ddf8e072cc
Meet Gandhi <meet.gandhi@simformsolutions.com>
2026-03-03 10:19:22 +0530

    TE-T291-calculator-userinterface

---
+++ modeSelection.js
@@ -0,0 +1,15 @@
+const selector = document.getElementById("modeSelector");
+const trigSection = document.getElementById("trigSection");
+const functionSection = document.getElementById("functionSection");
+
+selector.addEventListener("change", function () {
+    
+  trigSection.style.display = "none";
+  functionSection.style.display = "none";
+
+  if (this.value === "trig") {
+    trigSection.style.display = "flex";
+  } else if (this.value === "func") {
+    functionSection.style.display = "flex";
+  }
+});
